4d1ef648f988ca84a2b6034db2e787a34b32415a
Burak Sokullu <burak.sokullu@hepsiburada.com>
2020-09-06 20:34:57 +0300

    minor fixes

--- app/store/reducers/index.js
+++ app/store/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux-immutable';
-import { reducer as formReducer } from 'redux-form/immutable';
 import { reducer as toastrReducer } from 'react-redux-toastr';
 import eventsReducer from './eventsReducer';
 import citiesReducer from './citiesReducer';
 
 const rootReducer = combineReducers({
-  form: formReducer,
   toastr: toastrReducer,
   events: eventsReducer,
   cities: citiesReducer
